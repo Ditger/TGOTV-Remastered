714e8a83ce008027fc7ac944c202ca2aa6d909c9
DitgerDW <ditger.de.wolf@hotmail.com>
2020-07-22 15:17:12 +0200

    Some responsive fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
 
+$(document).ready(function() {
+    $(".btn-android").on("click", function () {
+        $(".android-phone").css("display", "inline-block");
+        $(".iphone").css("display", "none");
+
+    });
+
+    $(".btn-iphone").on("click", function () {
+        $(".iphone").css("display", "inline-block");
+        $(".android-phone").css("display", "none");
+    });
 });
